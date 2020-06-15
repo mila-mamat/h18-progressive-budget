4eb44babe894bb65c75e79c0412639ad741964f3
Mila <mila.windsor@gmail.com>
2020-06-15 17:47:59 -0400

    indexeddb is working
    adding transaction is working offline. But when it is online, needs to refresh twice to get the latest transactions

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,35 +1,41 @@
+import {
+  saveTransaction
+} from "./indexedDB.js";
+
+//create global variables
 let transactions = [];
 let myChart;
 
 fetch("/api/transaction")
-  .then(response => {
+  .then((response) => {
     return response.json();
   })
-  .then(data => {
-    // save db data on global variable
+  .then((data) => {
+    // Fetch and save the transactions from the database
     transactions = data;
-
     populateTotal();
     populateTable();
     populateChart();
   });
 
+// populate the total based on the transactions
 function populateTotal() {
   // reduce transaction amounts to a single total value
   let total = transactions.reduce((total, t) => {
     return total + parseInt(t.value);
   }, 0);
-
+  // insert total value
   let totalEl = document.querySelector("#total");
   totalEl.textContent = total;
 }
 
+// create a table based on transactions
 function populateTable() {
+  // Update the DOM
   let tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
 
-  transactions.forEach(transaction => {
-    // create and populate a table row
+  transactions.forEach((transaction) => {
     let tr = document.createElement("tr");
     tr.innerHTML = `
       <td>${transaction.name}</td>
@@ -40,44 +46,46 @@ function populateTable() {
   });
 }
 
+// create a line chart based on transactions
 function populateChart() {
   // copy array and reverse it
   let reversed = transactions.slice().reverse();
   let sum = 0;
 
-  // create date labels for chart
-  let labels = reversed.map(t => {
+ 
+  let labels = reversed.map((t) => {
     let date = new Date(t.date);
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   });
 
-  // create incremental values for chart
-  let data = reversed.map(t => {
+
+  let data = reversed.map((t) => {
     sum += parseInt(t.value);
     return sum;
   });
 
-  // remove old chart if it exists
+  
   if (myChart) {
     myChart.destroy();
   }
-
+ 
   let ctx = document.getElementById("myChart").getContext("2d");
 
   myChart = new Chart(ctx, {
-    type: 'line',
-      data: {
-        labels,
-        datasets: [{
-            label: "Total Over Time",
-            fill: true,
-            backgroundColor: "#6666ff",
-            data
-        }]
-    }
+    type: "line",
+    data: {
+      labels,
+      datasets: [{
+        label: "Total Over Time",
+        fill: true,
+        backgroundColor: "#6666ff",
+        data,
+      }, ],
+    },
   });
 }
 
+//send transaction on click of add/ subtract buttons
 function sendTransaction(isAdding) {
   let nameEl = document.querySelector("#t-name");
   let amountEl = document.querySelector("#t-amount");
@@ -87,19 +95,18 @@ function sendTransaction(isAdding) {
   if (nameEl.value === "" || amountEl.value === "") {
     errorEl.textContent = "Missing Information";
     return;
-  }
-  else {
+  } else {
     errorEl.textContent = "";
   }
 
-  // create record
+  // create new transaction
   let transaction = {
     name: nameEl.value,
     value: amountEl.value,
-    date: new Date().toISOString()
+    date: new Date().toISOString(),
   };
 
-  // if subtracting funds, convert amount to negative number
+  // if subtracting, convert amount to negative number
   if (!isAdding) {
     transaction.value *= -1;
   }
@@ -107,47 +114,51 @@ function sendTransaction(isAdding) {
   // add to beginning of current array of data
   transactions.unshift(transaction);
 
-  // re-run logic to populate ui with new record
+  // re-populate the table, chart and total in the website
   populateChart();
   populateTable();
   populateTotal();
-  
-  // also send to server
+
+  // send the transaction to save into database using post api 
   fetch("/api/transaction", {
-    method: "POST",
-    body: JSON.stringify(transaction),
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
-    }
-  })
-  .then(response => {    
-    return response.json();
-  })
-  .then(data => {
-    if (data.errors) {
-      errorEl.textContent = "Missing Information";
-    }
-    else {
+      method: "POST",
+      body: JSON.stringify(transaction),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      console.log("online success response:", response);
+      return response.json();
+    })
+    .then((data) => {
+      if (data.errors) {
+        errorEl.textContent = "Missing Information";
+      } else {
+        // clear form
+        nameEl.value = "";
+        amountEl.value = "";
+      }
+    })
+    //if app is offline and api post fails, save the transaction in indexedDB
+    .catch((err) => {
+      
+      console.log(err);
+
+      // save into indexedDB
+      saveTransaction(transaction);
+
       // clear form
       nameEl.value = "";
       amountEl.value = "";
-    }
-  })
-  .catch(err => {
-    // fetch failed, so save in indexed db
-    saveRecord(transaction);
-
-    // clear form
-    nameEl.value = "";
-    amountEl.value = "";
-  });
+    });
 }
 
-document.querySelector("#add-btn").onclick = function() {
+document.querySelector("#add-btn").onclick = function () {
   sendTransaction(true);
 };
 
-document.querySelector("#sub-btn").onclick = function() {
+document.querySelector("#sub-btn").onclick = function () {
   sendTransaction(false);
 };
--- public/assets/indexedDB.js
+++ public/assets/indexedDB.js
@@ -1,39 +1,70 @@
-export function useIndexedDb(databaseName, storeName, method, object) {
-  return new Promise((resolve, reject) => {
-    const request = window.indexedDB.open(databaseName, 1);
-    let db,
-      tx,
-      store;
+let db
+// create a new db request for the budget indexedDB
+const request = indexedDB.open('budget', 1)
 
-    request.onupgradeneeded = function(e) {
-      const db = request.result;
-      db.createObjectStore(storeName, { keyPath: "_id" });
-    };
+request.onupgradeneeded = function (event) {
+  // create object store called "pending" and set autoIncrement to true
+  const db = event.target.result
+  // Set auto increment to true for the pending store
+  db.createObjectStore('pending', { autoIncrement: true })
+}
 
-    request.onerror = function(e) {
-      console.log("There was an error");
-    };
+request.onsuccess = function (event) {
+  db = event.target.result
 
-    request.onsuccess = function(e) {
-      db = request.result;
-      tx = db.transaction(storeName, "readwrite");
-      store = tx.objectStore(storeName);
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase()
+  }
+}
 
-      db.onerror = function(e) {
-        console.log("error");
-      };
-      if (method === "put") {
-        store.put(object);
-      }
-      if (method === "get") {
-        const all = store.getAll();
-        all.onsuccess = function() {
-          resolve(all.result);
-        };
-      }
-      tx.oncomplete = function() {
-        db.close();
-      };
-    };
-  });
+request.onerror = function (event) {
+  console.log('Woops! ' + event.target.errorCode)
+}
+
+export function saveTransaction (newTransaction) {
+  // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(['pending'], 'readwrite')
+
+  // access your pending object store
+  const store = transaction.objectStore('pending')
+
+  // add record to your store with add method.
+  store.add(newTransaction)
+}
+
+// This is called with an online event listener
+function checkDatabase () {
+  // open a transaction on your pending db
+  const transaction = db.transaction(['pending'], 'readwrite')
+  // access your pending object store
+  const store = transaction.objectStore('pending')
+  // get all records from store and set to a variable
+  const getAll = store.getAll()
+  // if there a values in the indexedDB post them to the actual database
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(['pending'], 'readwrite')
+
+          // access your pending object store
+          const store = transaction.objectStore('pending')
+
+          // clear all items in the store once they have been posted to the database
+          store.clear()
+        })
+        .catch(err => console.log(err))
+    }
+  }
+
 }
